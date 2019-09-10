88628c25ecb0f2897bf4d6ff29c58ea5cf48c9f9
seirin <43994152+yumseirin@users.noreply.github.com>
2019-09-10 15:34:24 +0800

    update MyWebTwo add CountsListener and CountsDao
    添加监听器CountsListener和CountsDao

---
+++ MyWebTwo/src/listener/CountsListener.java
@@ -0,0 +1,38 @@
+package listener;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import dao.CountsDao;
+
+public class CountsListener implements ServletContextListener {
+
+	/**
+	 * 关闭前--从上下文中拿count放数据库中
+	 */
+	public void contextDestroyed(ServletContextEvent arg0) {
+
+		System.out.println("Destroyed...........");
+
+		ServletContext sc = arg0.getServletContext();
+		Object obj = sc.getAttribute("count");
+		if (obj != null) {
+			int count = Integer.parseInt(obj.toString());
+			CountsDao dao = new CountsDao();
+			dao.addCounts(count);
+		}
+	}
+
+	/**
+	 * 启动成功后--从数据库中拿count写入上下文中
+	 */
+	public void contextInitialized(ServletContextEvent arg0) {
+
+		System.out.println("init..............");
+
+		CountsDao dao = new CountsDao();
+		arg0.getServletContext().setAttribute("count", dao.selectCounts());
+	}
+
+}
