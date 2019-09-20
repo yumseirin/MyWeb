8afb38b75e074d0497853eafd0af8cd4738baa2c
seirin <43994152+yumseirin@users.noreply.github.com>
2019-09-20 23:45:43 +0800

    Create ajaxOne.sql

---
+++ sql/ajaxOne.sql
@@ -0,0 +1,20 @@
+CREATE TABLE area(
+ID number(5),
+NAME varchar2(10),
+STATUS NUMBER(5),
+PID NUMBER(5)
+);
+insert into AREAC values(1,'吉林省',1,0);
+insert into AREAC values(2,'辽宁省',1,0);
+insert into AREAC values(3,'长春市',2,1);
+insert into AREAC values(4,'吉林市',2,1);
+insert into AREAC values(5,'大连市',2,2);
+insert into AREAC values(6,'沈阳市',2,2);
+insert into AREAC values(7,'绿园区',3,3);
+insert into AREAC values(8,'高新区',3,3);
+insert into AREAC values(9,'龙潭区',3,4);
+insert into AREAC values(10,'蛟河市',3,4);
+insert into AREAC values(11,'中山区',3,5);
+insert into AREAC values(12,'长海县',3,5);
+insert into AREAC values(13,'皇姑区',3,6);
+insert into AREAC values(14,'新民市',3,6);
