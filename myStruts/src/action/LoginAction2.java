f1ef77ebb69f7876e4951412632b79c40959339a
seirin <43994152+yumseirin@users.noreply.github.com>
2019-10-08 09:07:31 +0800

    Update LoginAction2.java

--- myStruts/src/action/LoginAction2.java
+++ myStruts/src/action/LoginAction2.java
@@ -1,11 +1,16 @@
 package action;
 
+import java.util.Map;
+
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.apache.struts2.ServletActionContext;
 
 import vo.Person;
 
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ModelDriven;
 
 public class LoginAction2 implements ModelDriven<Person>{
@@ -17,9 +22,31 @@ public class LoginAction2 implements ModelDriven<Person>{
 		}
 	
 	public String login(){
-		HttpServletRequest request=ServletActionContext.getRequest();
+		//HttpServletRequest request=ServletActionContext.getRequest();
 		System.out.println(p.getUname()+","+p.getPwd());
-		request.setAttribute("msg","sbb");
+		//解耦：
+		ActionContext cont = ActionContext.getContext();
+		//request
+		Map<String,Object>request = (Map<String,Object>)cont.get("request");
+		request.put("requestJO", "requestJO");
+		//session
+		Map<String,Object>session=cont.getSession();
+		session.put("sessionJO","sessionJO");
+		//application
+		Map<String,Object>application=cont.getApplication();
+		application.put("applicationJO","applicationJO");
+		
+		//耦合
+		//request
+		HttpServletRequest request2 = ServletActionContext.getRequest();
+		request2.setAttribute("requestOH", "requestOH");
+		//session
+		HttpSession session2 = request2.getSession();
+		session2.setAttribute("sessionOH", "sessionOH");
+		//application == ServletContext
+		ServletContext cont2 = session2.getServletContext();
+		cont2.setAttribute("applicationOH", "applicationOH");
+		
 		return "success";
 	}
 	public String selectPerson(){
